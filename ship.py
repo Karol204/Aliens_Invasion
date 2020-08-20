c8e37d2b0682f0c3b707c83432b1991077d3d5d6
karol204 <krogalski204@gmail.com>
2020-08-20 18:03:47 +0200

    right moves

--- ship.py
+++ ship.py
@@ -15,6 +15,14 @@ class Ship():
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Ship move option
+        self.moving_right = False
+
+    def update(self):
+        """Update ship's location based on move option"""
+        if self.moving_right:
+            self.rect.centerx += 1
+
     def blitme(self):
         """Show actual position of ship"""
         self.screen.blit(self.image, self.rect)
